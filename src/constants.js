c6b5bbd6f6f8d9ee3405fd2f86c98d3a88304430
madhuri priyanka <susmithapriyanka03@gmail.com>
2021-09-11 10:15:46 +0530

    redeployment

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 const DEVELOPMENT_URL = "http://localhost:5454";
-const PRODUCTION_URL = " https://intense-cliffs-28848.herokuapp.com/";
+const PRODUCTION_URL = " https://intense-cliffs-28848.herokuapp.com";
 
 exports.API_URL =PRODUCTION_URL;
